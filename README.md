63a760ac0fcfc361c51541b555ff850791bf764c
Tero Paloheimo <tero.paloheimo@iki.fi>
2013-09-15 10:19:16 +0300

    Added stuff done this far

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ OL-playground
 =============
 
 Playground for testing OpenLayers and related libraries.
+
+Currently done
+--------------
+
+* Several map layers
+* Drawing features
---
+++ js/map.js
@@ -0,0 +1,134 @@
+
+// Coordinate transform from EPSG 3067 to EPSG 3857 using proj4js
+var coordTransform = function (coord) {
+	var epsg3067 = '+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs';
+	var epsg3857 = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs';
+
+	return proj4(epsg3067, epsg3857, coord);
+};
+
+var map, drawingLayer, controls;
+var initMap = function () {
+	map = new OpenLayers.Map('map', 
+		{
+			projection: 'EPSG:900913',
+			controls: [
+				new OpenLayers.Control.Navigation(),
+				new OpenLayers.Control.ArgParser(),
+				new OpenLayers.Control.Attribution(),
+				new OpenLayers.Control.MousePosition(),
+				new OpenLayers.Control.LayerSwitcher(),
+				new OpenLayers.Control.PanZoomBar()
+			]
+		}
+	);
+
+	var layerOsm = new OpenLayers.Layer.OSM('OSM');
+	var layerMmlBase = new OpenLayers.Layer.MML('Peruskartta', [
+		'http://tile1.kartat.kapsi.fi/1.0.0/peruskartta/${z}/${x}/${y}.png',
+		'http://tile2.kartat.kapsi.fi/1.0.0/peruskartta/${z}/${x}/${y}.png'
+	], {
+		numZoomLevels: 20,
+		sphericalMecator: true,
+		transitionEffect: 'resize'
+	});
+	var layerMmlOrto = new OpenLayers.Layer.MML('Ortokuva', [
+		'http://tile1.kartat.kapsi.fi/1.0.0/ortokuva/${z}/${x}/${y}.png',
+		'http://tile2.kartat.kapsi.fi/1.0.0/ortokuva/${z}/${x}/${y}.png'
+	], {
+		numZoomLevels: 20,
+		sphericalMecator: true,
+		transitionEffect: 'resize'
+	});
+	var layerMmlBackground = new OpenLayers.Layer.MML('Taustakartta', [
+		'http://tile1.kartat.kapsi.fi/1.0.0/taustakartta/${z}/${x}/${y}.png',
+		'http://tile2.kartat.kapsi.fi/1.0.0/taustakartta/${z}/${x}/${y}.png'
+	], {
+		numZoomLevels: 20,
+		sphericalMecator: true,
+		transitionEffect: 'resize'
+	});	
+	map.addLayers([layerOsm, layerMmlBase, layerMmlBackground, layerMmlOrto]);
+
+	map.setCenter(
+	    new OpenLayers.LonLat(2776269.5105216, 8437730.296525601).transform(
+	        new OpenLayers.Projection('EPSG:3857'),
+	        map.getProjectionObject()
+	    ), 15
+	);
+};
+initMap();
+
+// Setup of drawing tools
+var setupDrawing = function () {
+	drawingLayer = new OpenLayers.Layer.Vector('Drawing layer');
+	map.addLayer(drawingLayer);
+
+	var doneHandler = function(type) {
+		if (type !== 'centerLine') {
+			drawingLayer.removeAllFeatures();
+		} else {
+
+		}
+		controls[type].deactivate();
+	};
+
+	controls = {
+		standard: new  OpenLayers.Control.DrawFeature(drawingLayer,
+			OpenLayers.Handler.RegularPolygon,
+				{
+					handlerOptions: {
+						sides: 4
+					}
+				}),
+		rectangle: new  OpenLayers.Control.DrawFeature(drawingLayer,
+			OpenLayers.Handler.RegularPolygon,
+				{
+					handlerOptions: {
+						sides: 4
+					}
+				}),
+		polygon: new OpenLayers.Control.DrawFeature(drawingLayer,
+            OpenLayers.Handler.Polygon),
+		centerLine: new OpenLayers.Control.DrawFeature(drawingLayer,
+			OpenLayers.Handler.Path,
+			{
+				handlerOptions: {
+					maxVertices: 2
+				}
+			})
+	};
+	// Handlers
+	controls['rectangle'].events.on({
+		'featureadded': function () {
+			doneHandler('rectangle');
+		}
+	});
+	controls['polygon'].events.on({
+		'featureadded': function () {
+			doneHandler('polygon');
+		}
+	});
+	controls['centerLine'].events.on({
+		'featureadded': function () {
+			doneHandler('centerLine');
+		}
+	});
+
+	for(var key in controls) {
+        map.addControl(controls[key]);
+    }
+};
+setupDrawing();
+
+// Activates drawing
+var activateDrawing = function (type) {
+	controls[type].activate();
+	/*for(var key in controls) {
+		if (key === type) {
+			controls[key].activate();
+		} else {
+			controls[key].deactivate();
+		}
+	}*/
+};
